67dbc8540ae28462839dfc391aeca38a2e03323f
ElisaAAgostini <146785013+ElisaAAgostini@users.noreply.github.com>
2023-10-23 23:34:54 -0300

    atualizacao 2310/2

--- orderitem.py
+++ orderitem.py
@@ -3,8 +3,7 @@ class Orderitem {
 
     def__init__(self, quantity, unitary_price, order, product)
         self._quantity = quantity
-        self._unitary_price = unitary price
+        self._unitary_price = unitary_price
         self._order = order 
         self._product = product
-
 }
