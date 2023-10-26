7fdc0f67e49b47329d7263c4231e65b971f08b62
ElisaAAgostini <146785013+ElisaAAgostini@users.noreply.github.com>
2023-10-26 20:06:42 -0300

    atualizacao com correcao de erros

--- order.py
+++ order.py
@@ -1,7 +1,6 @@
-class Order {
-    
-    def__init__(self, total_price, status, client)
+class Order:  
+     
+    def__init__(self, total_price, total_price, status, client):
         self._total_price = total_price
         self._status = status
         self._client = client
-}
