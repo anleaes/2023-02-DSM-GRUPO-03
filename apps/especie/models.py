a36ab70e3bb5c27963280084ea4bc01b474e3c79
ThacielleVargas <thaciellec@gmail.com>
2023-11-27 18:54:57 -0300

    att

--- apps/especie/models.py
+++ apps/especie/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Especie(models.Model):
-    tipo = models.CharField('Tipo', max_length=15)
+    tipo = models.TextField('Tipo', max_length=15)
     raca = models.TextField('Raça', max_length=50)
-    porte = models.CharField('Porte', max_length=20)
+    porte = models.TextField('Porte', max_length=20)
 
     class Meta:
         verbose_name = 'especie'
