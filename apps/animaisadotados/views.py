5a26c4c6cc356f2e6020186234e2f177385930cc
AeroGamer01 <Lauro_carvalho@live.com>
2023-11-30 19:19:11 -0300

    Animais_adotadosViews

--- apps/animaisadotados/views.py
+++ apps/animaisadotados/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Animaisadotado
+from rest_framework import viewsets
+from .serializer import AnimaisadotadoSerializer
 
 # Create your views here.
+
+class AdocaoViewSet(viewsets.ModelViewSet):
+    queryset = Animaisadotado.objects.all()
+    serializer_class = AnimaisadotadoSerializer
